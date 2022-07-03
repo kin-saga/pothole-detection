a9a3309fad6a2e1c4b72ac9e4583181f9407eb7e
pee47kay <papakowdadson@gmail.com>
2022-07-03 20:21:22 +0100

    google api

--- g4app/lib/pages/splash.dart
+++ g4app/lib/pages/splash.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:g4app/pages/login.dart';
 // import 'package:http/http.dart';
 
 class splash extends StatefulWidget {
@@ -9,6 +10,16 @@ class splash extends StatefulWidget {
 }
 
 class _splashState extends State<splash> {
+  @override
+  void initState() {
+    Future.delayed(const Duration(seconds: 5),(){
+      Navigator.pushReplacement(context, MaterialPageRoute(builder: (context) => login()));
+
+    },);
+    // ignore: todo
+    // TODO: implement initState
+    super.initState();
+  }
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -38,6 +49,13 @@ class _splashState extends State<splash> {
 
           ),
           SizedBox(height: 10,),
+          const SizedBox(
+            height: 20,
+            width: 20,
+            child: CircularProgressIndicator(
+              strokeWidth: 2,
+            ),
+          ),
           Column(
             mainAxisAlignment: MainAxisAlignment.end,
             children: [
